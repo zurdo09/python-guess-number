81663c50b4d940947f8ce779007824326c857a39
Sebastian Ibarguen <s@cognits.co>
2014-11-23 19:23:45 -0600

    Initial commit

---
+++ tests.py
@@ -0,0 +1,13 @@
+import nose
+from pylint import epylint as lint
+
+APPLICATION_DIR = 'application.py'
+
+class TestApplication(unittest.TestCase):
+
+
+    def test_lint(self):
+        (pylint_stdout, pylint_stderr) = lint.py_run(APPLICATION_DIR, True)
+
+if __name__ == '__main__':
+    unittest.main()
