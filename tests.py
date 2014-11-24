6d2dfddac4aebbd1009ba1b559cebd9ccf35b73a
Ludwin Garcia <lgarcia@cognits.co>
2014-11-24 17:01:18 -0600

    Modificacion del Codigo
    Se modifico el código para ejecutar el test correctamente.

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
+import os
 import nose
 import unittest
-from pylint import epylint as lint
 
 APPLICATION_DIR = 'application.py'
 
@@ -8,7 +8,9 @@ class TestApplication(unittest.TestCase):
 
 
     def test_lint(self):
-        (pylint_stdout, pylint_stderr) = lint.py_run(APPLICATION_DIR, True)
+        resultado = os.system('pylint' + " " +APPLICATION_DIR)
+        return resultado
 
 if __name__ == '__main__':
     unittest.main()
+
